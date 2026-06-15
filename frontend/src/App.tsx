6d7743e2209d830151c55554e01a75e63034c411
Victor Von Doom <fabprime123@gmail.com>
2026-06-15 06:19:51 -0700

    Message 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
 function App() {
 
   return (
-    <>Hello</>
+    <>
+      <div>
+        <h2>{"MY APP"}</h2>
+        <header>
+          <Show when="signed-out">
+            <SignInButton mode="modal" />
+            <SignUpButton mode='modal' />
+          </Show>
+          <Show when="signed-in">
+            <UserButton />
+          </Show>
+        </header>
+      </div>
+    </>
   )
 }
 
